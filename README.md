7d89b68abcb1da0f94b488c9e70da681abfef002
Filter Cai <u2chcai@gmail.com>
2015-12-26 08:17:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repdata-P2
---
+++ data/getdata.sh
@@ -0,0 +1,7 @@
+wget https://d396qusza40orc.cloudfront.net/repdata/data/StormData.csv.bz2
+wget https://d396qusza40orc.cloudfront.net/repdata/peer2_doc/pd01016005curr.pdf
+wget https://d396qusza40orc.cloudfront.net/repdata/peer2_doc/NCDC%20Storm%20Events-FAQ%20Page.pdf
+mv NCDC*.pdf NCDC_Storm_Events-FAQ_Page.pdf
+bunzip2 StormData.csv.bz2
+
+
